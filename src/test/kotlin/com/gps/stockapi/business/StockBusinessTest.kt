c9485b485a7bee86e45da57eb45ab51b056f10fe
Gerffson <gpjunior@uolinc.com>
2021-09-26 16:44:14 -0300

    Revomendo dependencia do Jairo

--- src/test/kotlin/com/gps/stockapi/business/StockBusinessTest.kt
+++ src/test/kotlin/com/gps/stockapi/business/StockBusinessTest.kt
@@ -1,13 +1,10 @@
 package com.gps.stockapi.business
 
-import com.github.jairovsky.fixturefactory.kotlin.FixtureDefinitions
-import com.github.jairovsky.fixturefactory.kotlin.gimme
 import com.gps.stockapi.model.Stock
 import com.gps.stockapi.repository.StockRepository
-import com.nhaarman.mockito_kotlin.*
-import io.kotlintest.TestCase
+import com.nhaarman.mockito_kotlin.mock
+import com.nhaarman.mockito_kotlin.whenever
 import io.kotlintest.shouldBe
-import io.kotlintest.specs.StringSpec
 import org.junit.Before
 import org.junit.Test
 import org.mockito.ArgumentCaptor
@@ -28,7 +25,7 @@ class StockBusinessTest {
 
     @Test
     fun shouldAddProductToStock() {
-        val stock = gimme<Stock>("stock1")
+        val stock = Stock(id = 10, locationId = 20, productId = 30, quantity = 1.0)
         val captorStock = ArgumentCaptor.forClass(Stock::class.java)
 
         whenever(stockRepository.findByLocationIdAndProductId(anyInt(), anyInt())).thenReturn(Optional.of(stock))
@@ -39,35 +36,3 @@ class StockBusinessTest {
         result.quantity shouldBe 2.0
     }
 }
-
-
-//
-//class StockBusinessTest: StringSpec() {
-//    private var stockBusiness: StockBusiness = mock()
-//    private var stockRepository: StockRepository = mock()
-//
-//    override fun beforeTest(testCase: TestCase) {
-//        super.beforeTest(testCase)
-//        stockBusiness = mock()
-//        stockRepository = mock()
-//    }
-//
-//    init {
-//
-//        "Should add product to stock" {
-//            val stock = gimme<Stock>("stock1")
-//            val captorStock = ArgumentCaptor.forClass(Stock::class.java)
-//            whenever(stockRepository.findByLocationIdAndProductId(anyInt(), anyInt())).thenReturn(Optional.of(stock))
-//            whenever(stockRepository.save(captorStock.capture()  )).thenReturn(stock)
-//            val result = stockBusiness.addProductToStock(20, 30, 1.0)
-//            result.quantity shouldBe 2.0
-//        }
-//
-//    }
-//
-//}
-class Fixtures : FixtureDefinitions({
-    "stock1" {
-        Stock(id = 10, locationId = 20, productId = 30, quantity = 1.0)
-    }
-})
